f70777f3378d8a30a8e346d2a4fa67da8783b62c
ortava <RyanTommyTavares@gmail.com>
2025-01-07 16:23:58 -0800

    Add dao.py

---
+++ dao.py
@@ -0,0 +1,37 @@
+"""Data Access Layer for communicating directly with the MySQL database."""
+
+import mysql.connector.errors
+from connection import connect_to_mysql
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Dao():
+    def __init__(self):
+        self.cnx = connect_to_mysql()
+
+    def __del__(self):
+        self.cnx.close()
+        logger.info("DB connection closed")
+    
+    def insert_user(self, username, password, date_of_birth):
+        if self.cnx and self.cnx.is_connected():
+            with self.cnx.cursor() as cursor:
+                try:
+                    insert_query = "INSERT INTO Users (username, password, date_of_birth) VALUES (%s, %s, %s)"
+                    cursor.execute(insert_query, (username, password, date_of_birth))
+                    self.cnx.commit()
+                    logger.info("Inserted user %s", username)
+                    return True
+                except mysql.connector.Error as e:
+                    logger.warning("Failed to insert user %s :: %s", (username, e.msg))
+        return False
+    
+    def select_user_by_username(self, username):
+        if self.cnx and self.cnx.is_connected():
+            with self.cnx.cursor() as cursor:
+                try:
+                    cursor.execute("SELECT * FROM Users WHERE username=%s", [username])
+                    return cursor.fetchone()
+                except mysql.connector.Error as e:
+                    logger.warning("Query to select user by username (%s) failed :: %s", (username, e.msg))
