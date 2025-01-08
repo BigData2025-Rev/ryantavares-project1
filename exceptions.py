008cbd2e3d2c74fe9bcf3eca4dd6be7b07f30988
ortava <RyanTommyTavares@gmail.com>
2025-01-07 19:28:01 -0800

    Add InvalidInputError custom exception

--- exceptions.py
+++ exceptions.py
@@ -8,3 +8,14 @@ class AlreadyExistsError(Exception):
 
 class InvalidCredentialsError(Exception):
     pass
+
+class InvalidInputError(Exception):
+    def __init__(self, valid_keys: list[str], message=""):
+        self.valid_keys = valid_keys
+        self.message=message
+        
+    def __str__(self):
+        bold = "\033[1m"
+        red = '\033[91m'
+        end = "\033[0m"
+        return red + bold + "Invalid input. Try: " + ", ".join(self.valid_keys) + end + "\n"
